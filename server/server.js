7ecbc249370503b4388738e405100f2eb32c0bf6
seid <seyamen@gmail.com>
2026-09-14 12:56:40 +0300

    server

--- server/server.js
+++ server/server.js
@@ -9,12 +9,29 @@ app.get("/", (req, res) => {
   res.send("Backend is running 🚀");
 });
 
+//Liveness End Point
 app.get("/healthz", (req, res) => {
   res.status(200).send("OK");
 });
 
-app.get("/api/message", (req, res) => {
-  res.json({ message: "back end response👋" });
+// READINESS PROBE // 
+//Checks whether the application can communicate with PostgreSQL.
+ // If PostgreSQL is unavailable, the application is NOT ready
+ // to receive traffic.
+
+app.get("/ready", async (req, res) => {
+  try {
+    // Verify that the application can communicate with PostgreSQL
+    await db.query("SELECT 1");
+
+    // Database is reachable → application is ready
+    res.status(200).send("READY");
+  } catch (error) {
+    // Database is unavailable → application is not ready
+    console.error("Readiness check failed:", error.message);
+
+    res.status(503).send("NOT READY");
+  }
 });
 
 const PORT = 9000;
